ed821d3161d68bffb2902f6d4ecddc64c691f340
Kunal Nagar <knlnagar@gmail.com>
2019-06-16 05:19:23 +0530

    Fix formatting spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    jquery: true,
-    mocha: true
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    "no-unused-expressions": "off",
-    indent: ['error', 4]
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jquery: true,
+        mocha: true
+    },
+    extends: 'airbnb-base',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        "no-unused-expressions": "off",
+        indent: ['error', 4]
+    },
 };
